48cb624ee549bf26bb93f56fa6555d27949bca25
Favid <dladosky@gmail.com>
2017-04-29 15:21:36 -0400

    Added test project with first conversion test

---
+++ XcomPerkManagerTests/ConvertDriverToXml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XcomPerkManager.DataObjects;
+using System.Xml.Linq;
+using XcomPerkManager;
+
+namespace XcomPerkManagerTests
+{
+    [TestClass]
+    public class ConvertDriverToXml
+    {
+        [TestMethod]
+        public void convertWeapon()
+        {
+            Weapon weapon = new Weapon();
+            weapon.weaponName = "shotgun";
+            weapon.weaponSlot = "primary";        // TODO verify if it should be primary or eInv_PrimaryWeapon
+
+            XElement actual = XcomPerkManager.Conversion.Convert.toXmlWeapon(weapon);
+
+            XElement expected = new XElement(Constants.XML_EQUIPMENT_WEAPON);
+            expected.Add(new XElement(Constants.XML_EQUIPMENT_WEAPON_NAME, "shotgun"));
+            expected.Add(new XElement(Constants.XML_EQUIPMENT_WEAPON_SLOT, "primary"));
+
+            Assert.IsTrue(XNode.DeepEquals(expected, actual));
+        }
+    }
+}
