ead3aedbea3a9d5f61bf05c59a468aedad827a92
Favid <dladosky@gmail.com>
2017-05-13 13:44:06 -0400

    Built skeleton for Exporting

--- XcomPerkManager/Forms/ExportManager.Designer.cs
+++ XcomPerkManager/Forms/ExportManager.Designer.cs
@@ -28,107 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.chListClasses = new System.Windows.Forms.CheckedListBox();
+            this.tFolder = new System.Windows.Forms.TextBox();
+            this.bBrowse = new System.Windows.Forms.Button();
+            this.bExport = new System.Windows.Forms.Button();
+            this.laFolder = new System.Windows.Forms.Label();
+            this.gCompatibility = new System.Windows.Forms.GroupBox();
+            this.opLongWar2 = new System.Windows.Forms.RadioButton();
+            this.opVanilla = new System.Windows.Forms.RadioButton();
+            this.bClose = new System.Windows.Forms.Button();
+            this.gCompatibility.SuspendLayout();
             this.SuspendLayout();
             // 
-            // checkedListBox1
+            // chListClasses
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(27, 30);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(178, 259);
-            this.checkedListBox1.TabIndex = 0;
+            this.chListClasses.FormattingEnabled = true;
+            this.chListClasses.Location = new System.Drawing.Point(27, 30);
+            this.chListClasses.Name = "chListClasses";
+            this.chListClasses.Size = new System.Drawing.Size(178, 259);
+            this.chListClasses.TabIndex = 0;
             // 
-            // textBox1
+            // tFolder
             // 
-            this.textBox1.Location = new System.Drawing.Point(239, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(158, 20);
-            this.textBox1.TabIndex = 1;
+            this.tFolder.Location = new System.Drawing.Point(266, 30);
+            this.tFolder.Name = "tFolder";
+            this.tFolder.Size = new System.Drawing.Size(158, 20);
+            this.tFolder.TabIndex = 1;
             // 
-            // button1
+            // bBrowse
             // 
-            this.button1.Location = new System.Drawing.Point(403, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bBrowse.Location = new System.Drawing.Point(430, 28);
+            this.bBrowse.Name = "bBrowse";
+            this.bBrowse.Size = new System.Drawing.Size(75, 23);
+            this.bBrowse.TabIndex = 2;
+            this.bBrowse.Text = "Browse";
+            this.bBrowse.UseVisualStyleBackColor = true;
+            this.bBrowse.Click += new System.EventHandler(this.bBrowse_Click);
             // 
-            // button2
+            // bExport
             // 
-            this.button2.Location = new System.Drawing.Point(403, 77);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.bExport.Location = new System.Drawing.Point(349, 266);
+            this.bExport.Name = "bExport";
+            this.bExport.Size = new System.Drawing.Size(75, 23);
+            this.bExport.TabIndex = 7;
+            this.bExport.Text = "Export";
+            this.bExport.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // laFolder
             // 
-            this.textBox2.Location = new System.Drawing.Point(239, 79);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(158, 20);
-            this.textBox2.TabIndex = 3;
+            this.laFolder.AutoSize = true;
+            this.laFolder.Location = new System.Drawing.Point(221, 33);
+            this.laFolder.Name = "laFolder";
+            this.laFolder.Size = new System.Drawing.Size(39, 13);
+            this.laFolder.TabIndex = 28;
+            this.laFolder.Text = "Folder:";
             // 
-            // button3
+            // gCompatibility
             // 
-            this.button3.Location = new System.Drawing.Point(403, 103);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.gCompatibility.Controls.Add(this.opLongWar2);
+            this.gCompatibility.Controls.Add(this.opVanilla);
+            this.gCompatibility.Location = new System.Drawing.Point(27, 318);
+            this.gCompatibility.Name = "gCompatibility";
+            this.gCompatibility.Size = new System.Drawing.Size(200, 78);
+            this.gCompatibility.TabIndex = 31;
+            this.gCompatibility.TabStop = false;
+            this.gCompatibility.Text = "Compatibility";
             // 
-            // textBox3
+            // opLongWar2
             // 
-            this.textBox3.Location = new System.Drawing.Point(239, 105);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(158, 20);
-            this.textBox3.TabIndex = 5;
+            this.opLongWar2.AutoSize = true;
+            this.opLongWar2.Location = new System.Drawing.Point(6, 42);
+            this.opLongWar2.Name = "opLongWar2";
+            this.opLongWar2.Size = new System.Drawing.Size(81, 17);
+            this.opLongWar2.TabIndex = 32;
+            this.opLongWar2.TabStop = true;
+            this.opLongWar2.Text = "Long War 2";
+            this.opLongWar2.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // opVanilla
             // 
-            this.button4.Location = new System.Drawing.Point(349, 266);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.opVanilla.AutoSize = true;
+            this.opVanilla.Location = new System.Drawing.Point(6, 19);
+            this.opVanilla.Name = "opVanilla";
+            this.opVanilla.Size = new System.Drawing.Size(56, 17);
+            this.opVanilla.TabIndex = 31;
+            this.opVanilla.TabStop = true;
+            this.opVanilla.Text = "Vanilla";
+            this.opVanilla.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // bClose
             // 
-            this.button5.Location = new System.Drawing.Point(268, 266);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.bClose.Location = new System.Drawing.Point(266, 266);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(75, 23);
+            this.bClose.TabIndex = 32;
+            this.bClose.Text = "Close";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
             // 
             // ExportManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(608, 419);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.bClose);
+            this.Controls.Add(this.gCompatibility);
+            this.Controls.Add(this.laFolder);
+            this.Controls.Add(this.bExport);
+            this.Controls.Add(this.bBrowse);
+            this.Controls.Add(this.tFolder);
+            this.Controls.Add(this.chListClasses);
             this.Name = "ExportManager";
             this.Text = "ExportManager";
+            this.gCompatibility.ResumeLayout(false);
+            this.gCompatibility.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,14 +149,14 @@
 
         #endregion
 
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckedListBox chListClasses;
+        private System.Windows.Forms.TextBox tFolder;
+        private System.Windows.Forms.Button bBrowse;
+        private System.Windows.Forms.Button bExport;
+        private System.Windows.Forms.Label laFolder;
+        private System.Windows.Forms.GroupBox gCompatibility;
+        private System.Windows.Forms.RadioButton opLongWar2;
+        private System.Windows.Forms.RadioButton opVanilla;
+        private System.Windows.Forms.Button bClose;
     }
 }
--- XcomPerkManager/Forms/ExportManager.cs
+++ XcomPerkManager/Forms/ExportManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,78 @@ namespace XcomPerkManager.Forms
         {
             InitializeComponent();
         }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bBrowse_Click(object sender, EventArgs e)
+        {
+            ValidationResult validFolder = new ValidationResult();
+            validFolder.valid = false;
+
+            FolderBrowserDialog dialog;
+
+            do
+            {
+                dialog = new FolderBrowserDialog();
+                DialogResult dialogResult = dialog.ShowDialog(this);
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    validFolder = validateFolderPath(dialog.SelectedPath);
+
+                    if (!validFolder.valid)
+                    {
+                        MessageBox.Show(validFolder.message);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            } while (!validFolder.valid);
+
+            tFolder.Text = dialog.SelectedPath;
+        }
+
+        private ValidationResult validateFolderPath(string path)
+        {
+            ValidationResult validFolderPath = new ValidationResult();
+            validFolderPath.valid = true;
+            validFolderPath.message = "";
+
+            List<string> fileNames = Directory.GetFiles(path).ToList();
+            
+            foreach(string fileName in fileNames)
+            {
+                int xComClassDataStringCompareResult = String.Compare(fileName, "XComClassData.ini", StringComparison.OrdinalIgnoreCase);
+                if(xComClassDataStringCompareResult == 0)
+                {
+                    validFolderPath.valid = false;
+                    validFolderPath.message += "This folder already contains XComClassData.init.";
+                    validFolderPath.message += "\n";
+                }
+
+                int xComGameIntStringCompareResult = String.Compare(fileName, "XComGame.INT", StringComparison.OrdinalIgnoreCase);
+                if (xComGameIntStringCompareResult == 0)
+                {
+                    validFolderPath.valid = false;
+                    validFolderPath.message += "This folder already contains XComGame.INT.";
+                    validFolderPath.message += "\n";
+                }
+            }
+
+            if(!validFolderPath.valid)
+            {
+                validFolderPath.message += "\n";
+                validFolderPath.message += "Please choose a new folder.";
+            }
+
+            return validFolderPath;
+        }
+
+        
     }
 }
